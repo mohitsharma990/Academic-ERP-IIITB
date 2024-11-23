3a546beee10f6b8b1f45986c4210c387212c9a79
mohitsharma990 <iitgmohitsharma@gmail.com>
2024-11-24 03:59:15 +0530

    Dept, emp, usr repos created.

--- Backend/AcademicERP/src/main/java/com/mohit/academicerp/repositories/EmployeeRepository.java
+++ Backend/AcademicERP/src/main/java/com/mohit/academicerp/repositories/EmployeeRepository.java
@@ -1,4 +1,22 @@
 package com.mohit.academicerp.repositories;
 
-public interface EmployeeRepository {
+import com.mohit.academicerp.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    @Query("SELECT e FROM employee e JOIN e.department d WHERE d.id = ?1")
+    List<Employee> findByDepartmentId(int id);
+
+    @Query("SELECT e FROM employee e WHERE e.email = ?1")
+    Optional<Employee> findByEmail(String email);
+
+    @Query("SELECT count(e) FROM employee e WHERE e.department.id = ?1")
+    int countByDepartmentId(int id);
 }
--- Backend/AcademicERP/src/main/java/com/mohit/academicerp/repositories/UserRepository.java
+++ Backend/AcademicERP/src/main/java/com/mohit/academicerp/repositories/UserRepository.java
@@ -1,4 +1,18 @@
 package com.mohit.academicerp.repositories;
 
-public class UserRepository {
+import com.mohit.academicerp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("SELECT u FROM users u JOIN u.employee e WHERE u.password = ?2 AND e.email = ?1")
+    Optional<User> validateUser(String email, String password);
+
+    @Query("SELECT u FROM users u JOIN u.employee e WHERE e.email = ?1")
+    Optional<User> findUserByEmail(String email);
 }
