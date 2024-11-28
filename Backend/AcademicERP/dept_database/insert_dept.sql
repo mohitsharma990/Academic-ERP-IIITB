8b9be9c963d2e675beea8f092cde7b20c70330ed
mohitsharma990 <iitgmohitsharma@gmail.com>
2024-11-28 15:08:54 +0530

    SQL files added

---
+++ Backend/AcademicERP/dept_database/insert_dept.sql
@@ -0,0 +1,47 @@
+insert into departments (dept_name) values
+('Accounts'),
+('HR'),
+('Faculty_CS'),
+('Faculty_ECE'),
+('Outreach'),
+('Administration'),
+('Admissions'),
+('Library'),
+('Sports'),
+('IT Services');
+
+
+
+INSERT INTO employees (f_name, l_name, email, title, photo_path, dept_id) VALUES
+    ('John', 'Doe', 'john.doe@example.com', 'Accountant', '/photos/john.jpg', 1),
+    ('Jane', 'Doe', 'jane.doe@example.com', 'HR Specialist', '/photos/jane.jpg', 2),
+    ('Ravi', 'Kumar', 'ravi.kumar@example.com', 'Faculty', '/photos/ravi.jpg', 1),
+    ('Priya', 'Sharma', 'priya.sharma@example.com', 'Faculty', '/photos/priya.jpg', 3),
+    ('Amit', 'Patel', 'amit.patel@example.com', 'Faculty', '/photos/amit.jpg', 4),
+    ('Neha', 'Singh', 'neha.singh@example.com', 'Outreach Coordinator', '/photos/neha.jpg', 5),
+    ('Rahul', 'Verma', 'rahul.verma@example.com', 'Faculty', '/photos/rahul.jpg', 1),
+    ('Sneha', 'Gupta', 'sneha.gupta@example.com', 'Sports Coordinator', '/photos/sneha.jpg', 5),
+    ('Vikas', 'Yadav', 'vikas.yadav@example.com', 'IT Specialist', '/photos/vikas.jpg', 6),
+    ('Anita', 'Rajput', 'anita.rajput@example.com', 'Administrator', '/photos/anita.jpg', 7),
+    ('Arun', 'Sharma', 'arun.sharma@example.com', 'Librarian', '/photos/arun.jpg', 8),
+    ('Swati', 'Goyal', 'swati.goyal@example.com', 'Sports Coordinator', '/photos/swati.jpg', 9),
+    ('Rajesh', 'Mishra', 'rajesh.mishra@example.com', 'IT Specialist', '/photos/rajesh.jpg', 10),
+    ('Pooja', 'Jha', 'pooja.jha@example.com', 'HR Specialist', '/photos/pooja.jpg', 2),
+    ('Rohit', 'Thakur', 'rohit.thakur@example.com', 'HOD', '/photos/rohit.jpg', 1),
+    ('Kavita', 'Verma', 'kavita.verma@example.com', 'IT Specialist', '/photos/kavita.jpg', 10),
+    ('Sachin', 'Yadav', 'sachin.yadav@example.com', 'Sports Coordinator', '/photos/sachin.jpg', 9),
+    ('Anjali', 'Gupta', 'anjali.gupta@example.com', 'HR Specialist', '/photos/anjali.jpg', 2),
+    ('Vivek', 'Kumar', 'vivek.kumar@example.com', 'IT Specialist', '/photos/vivek.jpg', 6),
+    ('Divya', 'Singh', 'divya.singh@example.com', 'Outreach Coordinator', '/photos/divya.jpg', 5),
+    ('Sanjay', 'Verma', 'sanjay.verma@example.com', 'Faculty', '/photos/sanjay.jpg', 3),
+    ('Sarita', 'Yadav', 'sarita.yadav@example.com', 'HR Specialist', '/photos/sarita.jpg', 2),
+    ('Mohan', 'Gupta', 'mohan.gupta@example.com', 'IT Specialist', '/photos/mohan.jpg', 10),
+    ('Anita', 'Kumari', 'anita.kumari@example.com', 'Faculty', '/photos/anita_kumari.jpg', 3),
+    ('Ajay', 'Sharma', 'ajay.sharma@example.com', 'Accountant', '/photos/ajay.jpg', 1),
+    ('Preeti', 'Yadav', 'preeti.yadav@example.com', 'HOD', '/photos/preeti.jpg', 4),
+    ('Sunil', 'Verma', 'sunil.verma@example.com', 'Administrator', '/photos/sunil.jpg', 7),
+    ('Neha', 'Singh', 'nha.singh@example.com', 'Administrator', '/photos/neha2.jpg', 7);
+
+
+
+
